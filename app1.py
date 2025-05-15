deff32ace26f550cdd4399686016fd2d4322b5dc
SOHAM SANTOSH KALGUTKAR <sohamkalg@gmail.com>
2025-05-15 17:20:35 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-BASE_URL = "http://127.0.0.1:5000"
+BASE_URL = "https://smartstocksuggester.onrender.com"
 
 st.set_page_config(page_title="Smart Stock Suggester", layout="wide")
 
